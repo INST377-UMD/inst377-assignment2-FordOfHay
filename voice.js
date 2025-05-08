81edc35f0be1c01dd55292d7701b5daad6cb7e73
FordOfHay <hdogbe@umd.edu>
2025-05-07 20:44:34 -0400

    second

--- voice.js
+++ voice.js
@@ -3,14 +3,24 @@ function startVoice() {
   if (annyang) {
     const commands = {
       'hello': () => alert('Hello World'),
+      
       'change the color to *color': (color) => {
         document.body.style.backgroundColor = color;
       },
+      
       'navigate to *page': (page) => {
         const dest = page.toLowerCase();
         if (dest.includes('home')) window.location.href = 'index.html';
         else if (dest.includes('stocks')) window.location.href = 'stocks.html';
         else if (dest.includes('dogs')) window.location.href = 'dogs.html';
+      },
+
+      'load dog breed *breed': (breed) => {
+        if (typeof loadBreedInfo === 'function') {
+          loadBreedInfo(breed); // Calls the function from dogs.js
+        } else {
+          alert("Sorry, I can't load that breed right now.");
+        }
       }
     };
 
